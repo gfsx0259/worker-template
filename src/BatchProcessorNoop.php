a6554b5b39bea7a3cc46049a7212b2929f410179
Konstantin Popov <kipopov@groscloud.com>
2026-04-17 17:38:15 +0300

    Do not make dummy readonly

--- src/BatchProcessorNoop.php
+++ src/BatchProcessorNoop.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Enthusiast\WorkerTemplate;
 
-readonly final class BatchProcessorNoop implements BatchProcessorInterface
+final class BatchProcessorNoop implements BatchProcessorInterface
 {
    use BatchProcessorLifecycleDefaults;
 }
